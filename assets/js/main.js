517a45630a99114a0afd49b1ccc0592a97fb5e0e
Maria Gomes <mariaergsantana@outlook.com>
2025-07-09 20:59:03 -0300

    Overall update

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,14 @@ function scrollTop() {
 window.addEventListener('scroll', scrollTop)
 
 /* MIXITUP FILTER PORTFOLIO */
-
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
 
 
 /* LINK ACTIVE PORTFOLIO */
